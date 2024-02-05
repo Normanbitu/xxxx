3ad21db04ba59bb35bd0ceb4aa39884b71f141c6
Normanbitu <157604384+Normanbitu@users.noreply.github.com>
2024-02-05 13:14:25 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,3 @@
+
+print("ISLAMIC UNIVERSITY IN UGANDA KAMPALA CAMPUS \n\tNAME:\tBatukirewa Norman\n\tREGNO:\t223-063011-25291\n\tlecturer: adbulhakim\n\tdate:\t24/01/2024\n\tcourseunit\t8\n\t\t\t\t***END***")
+
